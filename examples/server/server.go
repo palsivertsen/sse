9ff8aec038740396a641d680b2c7a2ef1b42201c
Pal Sivertsen <palsivertsen@gmail.com>
2017-09-06 13:20:52 +0200

    Made example more verbose

--- examples/server/server.go
+++ examples/server/server.go
@@ -8,19 +8,23 @@ import (
 	"github.com/palsivertsen/sse"
 )
 
+var addr = ":8080"
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", serveScript)
 	mux.HandleFunc("/stream", func(w http.ResponseWriter, req *http.Request) {
+		log.Printf("Client %s connected", req.RemoteAddr)
 		stream := sse.NewStream()
 		defer stream.Close()
 		go func() {
 			for {
 				select {
 				case <-stream.CloseNotify():
+					log.Printf("Client %s disconnected", req.RemoteAddr)
 					return
 				default:
-					log.Print("Sending sse message")
+					log.Print("Sending sse message to ", req.RemoteAddr)
 					stream.Send(sse.Event{
 						Message: "The time is: " + time.Now().String(),
 					})
@@ -30,7 +34,8 @@ func main() {
 		}()
 		stream.ServeHTTP(w, req)
 	})
-	http.ListenAndServe(":8080", mux)
+	log.Printf("A live demo is shortly available at %s", addr)
+	http.ListenAndServe(addr, mux)
 }
 
 func serveScript(w http.ResponseWriter, r *http.Request) {
